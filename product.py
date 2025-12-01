9063da433fe0f6d1323fc81752bf4ca39e9c812c
Prashantism12 <pashu.s.madival@gmail.com>
2025-12-01 15:35:21 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,14 @@
+def product_details(name, prod_id, quantity, price):
+    result = (
+        f"Product Name   : {name}\n"
+        f"Product ID     : {prod_id}\n"
+        f"Quantity     : {quantity}\n"
+        f"Price          : {price}"
+    )
+    return result
+if __name__ == "__main__":
+    name="Laptop"
+    prod_id="F1007"
+    quantity=1
+    price=95000
+    print(product_details(name, prod_id, quantity, price))
